56b1847f141e5957832d226c5c26cc4dcebc8ef8
Susan Carrera <su.susan89@gmail.com>
2021-12-05 06:50:30 -0500

    Add files via upload

---
+++ site.js
@@ -0,0 +1,102 @@
+//change let to var
+
+var playBestieFloppedKatya = function(){document.getElementById("BestieFloppedKatyaAudio").play()}
+// var playCorrectToWin = function(){document.getElementById("correctToWinaudio").play()}
+var playCorrectToWinWillam  = function(){document.getElementById("CorrectToWinWillamAudio").play()}
+
+// let play = function(){document.getElementById("audio").play()}
+
+var playTonePointed = function(){document.getElementById("tonePointedAudio").play()}
+
+
+var playAnnaOop  = function(){document.getElementById("AnnaOopAudio").play()}
+
+
+var playBackAgainAlyssa  = function(){document.getElementById("BackAgainAlyssaAudio").play()}
+
+
+var playBestieFloppedKatya  = function(){document.getElementById("BestieFloppedKatyaAudio").play()}
+
+
+var playBitchAlyssa  = function(){document.getElementById("BitchAlyssaAudio").play()}
+
+
+var playCorrectToWinWillam  = function(){document.getElementById("CorrectToWinWillamAudio").play()}
+
+
+var playDidntAskGaga  = function(){document.getElementById("DidntAskGagaAudio").play()}
+
+
+var playDontFuckItUp  = function(){document.getElementById("DontFuckItUpAudio").play()}
+
+
+var playfeelingMyOats  = function(){document.getElementById("feelingMyOatsAudio").play()}
+
+
+var playfunnyRuPaul  = function(){document.getElementById("funnyRuPaulAudio").play()}
+
+
+var playGagaTalented  = function(){document.getElementById("GagaTalentedAudio").play()}
+
+
+var playgarbageAlyssa  = function(){document.getElementById("GarbageAlyssaAudio").play()}
+
+
+var playGimmieMore  = function(){document.getElementById("GimmieMoreAudio").play()}
+
+
+var playGirlByeAlyssa  = function(){document.getElementById("GirlByeAlyssaAudio").play()}
+
+
+var playHadHerses  = function(){document.getElementById("HadHersesAudio").play()}
+
+
+var playHersesAlaska  = function(){document.getElementById("HersesAlaskaAudio").play()}
+
+
+var playhoneyTrixie  = function(){document.getElementById("HoneyTrixieAudio").play()}
+
+
+var playJodieFoster  = function(){document.getElementById("JodieFosterAudio").play()}
+
+
+var playLaughBob  = function(){document.getElementById("LaughBobAudio").play()}
+
+
+var playOhGurl  = function(){document.getElementById("OhGurlAudio").play()}
+
+
+var playOkWerk  = function(){document.getElementById("OkWerkAudio").play()}
+
+
+var playParty  = function(){document.getElementById("PartyAudio").play()}
+
+
+var playPerfectBeautiful  = function(){document.getElementById("PerfectBeautifulAudio").play()}
+
+
+var playprivledgeGia  = function(){document.getElementById("privledgeGiaAudio").play()}
+
+
+var playShade  = function(){document.getElementById("ShadeAudio").play()}
+
+
+var playSlave4U  = function(){document.getElementById("Slave4UAudio").play()}
+
+
+var playStepYourPussyUp  = function(){document.getElementById("StepYourPussyUpAudio").play()}
+
+
+var playSugarDaddyShangela  = function(){document.getElementById("SugarDaddyShangelaAudio").play()}
+
+
+var playTheyGotMeGal  = function(){document.getElementById("TheyGotMeGalAudio").play()}
+
+
+var playTonePointed  = function(){document.getElementById("TonePointedAudio").play()}
+
+
+var playwtfAlyssa  = function(){document.getElementById("wtfAlyssaAudio").play()}
+
+
+var playyasbitchworkGia  = function(){document.getElementById("yasbitchworkGiaAudio").play()}
